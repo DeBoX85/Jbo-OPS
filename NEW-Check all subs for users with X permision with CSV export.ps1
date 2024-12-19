d8b00ebfb9e615b9f18ff9095cd7bef8d269b239
Denis Bogunic <134433647+DeBoX85@users.noreply.github.com>
2024-12-19 21:24:47 +0100

    Update NEW-Check all subs for users with X permision with CSV export.ps1

--- NEW-Check all subs for users with X permision with CSV export.ps1
+++ NEW-Check all subs for users with X permision with CSV export.ps1
@@ -5,10 +5,10 @@
 #****Choose**** :
 
 # Get the user object for the user based on mail
-#$user = Get-AzADUser -UserPrincipalName 'adm-debo@rc-n.no'
+#$user = Get-AzADUser -UserPrincipalName 'adm@abc.com'
 
 # Get the user object for the user based in display name
-#$user = Get-AzADUser -SearchString "displayName eq 'Denis Bogunic'"
+#$user = Get-AzADUser -SearchString "displayName eq 'John Smith"
 
 
 # Initialize an array to store the results
